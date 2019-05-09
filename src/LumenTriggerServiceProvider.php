83248c687bb522e743be2b92e3e77d29f8dc20d0
huangdijia <huangdijia@gmail.com>
2019-05-09 13:20:44 +0800

    init

--- src/LumenTriggerServiceProvider.php
+++ src/LumenTriggerServiceProvider.php
@@ -18,11 +18,9 @@ class LaravelTriggerServiceProvider extends ServiceProvider
 
     public function configure()
     {
-        $this->mergeConfigFrom(
-            __DIR__.'/../config/trigger.php', 'trigger'
-        );
+        $this->mergeConfigFrom(__DIR__ . '/../../config/trigger.php', 'trigger');
         if ($this->app->runningInConsole()) {
-            $this->publishes([__DIR__ . '/../config/trigger.php' => config_path('trigger.php')]);
+            $this->publishes([__DIR__ . '/../../config/trigger.php' => config_path('trigger.php')]);
         }
     }
 
