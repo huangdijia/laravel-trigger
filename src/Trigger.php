e3042cac0236c6c24af901327a7b78fafa2d1991
huangdijia <huangdijia@gmail.com>
2019-05-30 08:48:13 +0800

    增加 Trigger 路由监听

--- src/Trigger.php
+++ src/Trigger.php
@@ -19,27 +19,92 @@ class Trigger
      * 绑定事件
      *
      * @param string $table
-     * @param string $eventType
-     * @param mixed $action
+     * @param string|array $eventType
+     * @param Closure|array|string|callable $action
      * @return void
      */
-    public function on(string $table, string $eventType, $action)
+    public function on(string $table, $eventType, $action = null)
     {
+        // table as db.tb1,db.tb2,...
+        if (false !== strpos($table, ',')) {
+            collect(explode(',', $table))->transform(function ($table) {
+                return trim($table);
+            })
+                ->filter()
+                ->each(function ($table) use ($eventType, $action) {
+                    $this->on($table, $eventType, $action);
+                });
+            return;
+        }
+
+        // * to *.*
         if ($table == '*') {
             $table .= '.*';
         }
 
-        $eventType = strtolower($eventType);
+        // default database
+        $table = ltrim($table, '.');
+        if (false === strpos($table, '.')) { // table to database.table
+            $table = sprintf('%s.%s', (config('trigger.databases')[0] ?? '*'), $table);
+        } elseif (substr($table, -1) == '.') { // database. to database.*
+            $table .= '*';
+        }
 
-        if (strpos($eventType, '|')) {
-            collect(explode('|', $eventType))->filter()->each(function ($eventType) use ($table, $action) {
+        // eventType as array
+        if (is_array($eventType)) {
+            collect($eventType)->each(function ($action, $eventType) use ($table) {
                 $this->on($table, $eventType, $action);
             });
 
             return;
         }
 
-        Arr::set($this->events, $table . '.' . $eventType, $action);
+        // eventType as string
+        if (is_string($eventType)) {
+            // to lower
+            $eventType = strtolower($eventType);
+
+            // eventType as write,update,delete...
+            if (false !== strpos($eventType, ',')) {
+                collect(explode(',', $eventType))
+                    ->transform(function ($eventType) {
+                        return trim($eventType);
+                    })
+                    ->filter()
+                    ->each(function ($eventType) use ($table, $action) {
+                        $this->on($table, $eventType, $action);
+                    });
+
+                return;
+            }
+        }
+
+        $key = sprintf('%s.%s', $table, $eventType);
+
+        // append to actions
+        $actions   = Arr::get($this->events, $key) ?: [];
+        $actions[] = $action;
+
+        // restore to array
+        Arr::set($this->events, $key, $actions);
+
+        return;
+    }
+
+    /**
+     * 执行事件
+     *
+     * @param mixed $events
+     * @param \MySQLReplication\Event\DTO\EventDTO $event
+     * @return void
+     */
+    public function trigger($events, EventDTO $event = null)
+    {
+        collect($events)->each(function ($e) use ($event) {
+            collect(Arr::get($this->events, $e))->each(function ($action) use ($event) {
+                $this->call($this->parseAction($action), [$event]);
+            });
+        });
 
         return;
     }
@@ -52,25 +117,21 @@ class Trigger
      */
     public function fire(EventDTO $event)
     {
-        $keys      = [];
+        $events    = [];
         $eventType = $event->getType();
 
         if (is_callable([$event, 'getTableMap'])) {
             $database = $event->getTableMap()->getDatabase();
             $table    = $event->getTableMap()->getTable();
-            $keys[]   = sprintf('%s.%s.%s', $database, $table, $eventType);
-            $keys[]   = sprintf('%s.%s.%s', $database, $table, '*');
-            $keys[]   = sprintf('%s.%s.%s', $database, '*', $eventType);
+            $events[] = sprintf('%s.%s.%s', $database, $table, $eventType);
+            $events[] = sprintf('%s.%s.%s', $database, $table, '*');
+            $events[] = sprintf('%s.%s.%s', $database, '*', $eventType);
         }
 
-        $keys[] = "*.*.{$eventType}";
-        $keys[] = "*.*.*";
+        $events[] = "*.*.{$eventType}";
+        $events[] = "*.*.*";
 
-        collect($keys)->each(function ($key) use ($event) {
-            collect(Arr::get($this->events, $key))->each(function ($action) use ($event) {
-                $this->call($this->parseAction($action), [$event]);
-            });
-        });
+        $this->trigger($events, $event);
 
         return;
     }
