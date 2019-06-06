f9e3c0cdd24056f6e4999eb68f5c5c0e04192da2
huangdijia <huangdijia@gmail.com>
2019-06-06 18:05:03 +0800

    optimize output

--- src/Console/ListCommand.php
+++ src/Console/ListCommand.php
@@ -5,6 +5,7 @@ namespace Huangdijia\Trigger\Console;
 use Closure;
 use Huangdijia\Trigger\Facades\Trigger;
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Arr;
 
 class ListCommand extends Command
@@ -28,10 +29,21 @@ class ListCommand extends Command
      */
     public function handle()
     {
-        $actions = collect(Arr::dot(Trigger::getEvents()))
-            ->transform(function ($action, $key) {
-                [$database, $table, $event, $num, $action] = explode('.', $key . '.' . $this->actionToString($action));
+        $actions = Trigger::getEvents();
+
+        collect(Arr::dot($actions))
+            ->transform(function ($action, $key) use ($actions) {
+                [$database, $table, $event, $num, $action] = explode('.', $key . '.' . $this->transformActionToString($action));
+
+                $key = sprintf('%s.%s.%s.%s', $database, $table, $event, $num);
+
+                if (is_numeric($action)) {
+                    $action = Arr::get($actions, $key);
+                    $action = $this->transformActionToString($action);
+                }
+
                 return [
+                    'key'      => $key,
                     'database' => $database,
                     'table'    => $table,
                     'event'    => $event,
@@ -48,6 +60,7 @@ class ListCommand extends Command
             ->when($this->option('event'), function ($collection, $event) {
                 return $collection->where('event', $event);
             })
+            ->unique('key')
             ->transform(function ($item) {
                 return [
                     $item['database'],
@@ -56,22 +69,30 @@ class ListCommand extends Command
                     $item['num'],
                     $item['action'],
                 ];
+            })
+            ->tap(function ($items) {
+                $this->table(['Database', 'Table', 'Event', 'Num', 'Action'], $items);
             });
-
-        $this->table(['Database', 'Table', 'Event', 'Num', 'Action'], $actions);
     }
 
-    public function actionToString($action)
+    public function transformActionToString($action)
     {
         if ($action instanceof Closure) {
             $action = 'Closure';
         } elseif (is_object($action)) {
             $action = get_class($action);
         } elseif (is_array($action)) {
+            if (is_object($action[0])) {
+                $action[0] = get_class($action[0]);
+            }
             $action = sprintf('%s@%s', $action[0], $action[1] ?? 'handle');
         } elseif (is_string($action)) {
             if (false === strpos($action, '@')) {
-                $action .= '@handle';
+                if (is_subclass_of($action, ShouldQueue::class)) {
+                    //
+                } else {
+                    $action .= '@handle';
+                }
             }
         }
 
