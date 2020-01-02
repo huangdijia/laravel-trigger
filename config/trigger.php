daf59c055a4ca03ee21f64a090882041da59b5f1
huangdijia <huangdijia@gmail.com>
2020-01-02 11:37:31 +0800

    add trigger:status

--- config/trigger.php
+++ config/trigger.php
@@ -11,7 +11,7 @@ return [
             'password'    => env('TRIGGER_PASSWORD', ''),
 
             // detect from trigger routers
-            'detect'      => false,
+            'detect'      => (bool) env('TRIGGER_DETECT', false),
             // or set database and tables
             'databases'   => env('TRIGGER_DATABASES', '') ? explode(',', env('TRIGGER_DATABASES')) : [],
             'tables'      => env('TRIGGER_TABLES', '') ? explode(',', env('TRIGGER_TABLES')) : [],
