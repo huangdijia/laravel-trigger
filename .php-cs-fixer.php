cd91991ad1292c6f1dc35101c8a251adc45511d1
Deeka Wong <huangdijia@gmail.com>
2022-12-09 12:00:49 +0800

    Adds php82 support (#3)
    * Adds PHP 8.2 support
    
    Co-authored-by: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -48,6 +48,11 @@ return (new PhpCsFixer\Config())
                 'author',
             ],
         ],
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => true,
+            'import_functions' => null,
+        ],
         'ordered_imports' => [
             'imports_order' => [
                 'class', 'function', 'const',
